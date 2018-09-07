e90f4f951e37cefdb810fd27c76e13df400490af
apu <pasha_anik@ukr.net>
2018-09-07 17:28:46 +0300

    Fix bug - I had wrong timeout results because I use System.out and write to log beetween send to socket and receive from it

--- src/main/java/com/apu/TcpClientForAccessControl/App.java
+++ src/main/java/com/apu/TcpClientForAccessControl/App.java
@@ -1,5 +1,7 @@
 package com.apu.TcpClientForAccessControl;
 
+import com.apu.TcpServerForAccessControlAPI.packet.AccessPacket;
+import com.apu.TcpServerForAccessControlAPI.packet.EventType;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -15,10 +17,14 @@ import com.apu.TcpServerForAccessControlAPI.packet.ServicePacket;
 import java.io.InputStream;
 import java.io.ObjectInput;
 import java.io.ObjectInputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
 
 
 /**
@@ -27,6 +33,10 @@ import java.util.logging.Logger;
  */
 public class App 
 {
+    public static boolean TEST_MODE = false;
+    
+    private static final Logger logger = LogManager.getLogger(App.class);
+    
     private static Socket socket; 
     private static final int CONNECTION_PORT = 65530;
     private static final String CONNECTION_HOST = "127.0.0.1";
@@ -34,6 +44,25 @@ public class App
     
     public static void main( String[] args )
     {
+        int deviceNumber;
+        if(TEST_MODE == false) {
+//        System.setOut(new PrintStream(new LoggingOutputStream(LogManager.getLogger("outLog"), Level.ALL), true));
+        
+            if(args.length == 0) {
+                System.out.print("Enter device number");
+                return;
+            }
+
+
+            try {
+                deviceNumber = Integer.parseInt(args[0]);
+            } catch(NumberFormatException ex) {
+                System.out.print("Error device number");
+                return;
+            }        
+        } else {
+            deviceNumber = 15;
+        }
         OutputStream os = null;
         InputStream is = null;
         
@@ -43,31 +72,43 @@ public class App
             os = socket.getOutputStream();
             is = socket.getInputStream();
         
-            RawPacket packet = new InfoPacket();
-            packet.setDeviceId(15);            
+            AccessPacket packet = new AccessPacket();
+            packet.setEventId(EventType.ENTER_QUERY.getIndex());
+            packet.setDeviceNumber(deviceNumber); 
+            packet.setCardNumber("11111111");            
             
             byte[] packetBytes;            
-            int packetNumber = 0;
-            
+            int packetNumber = 25;
+            RawPacket resultPacket;
+            long timeStart;
+            long timeFinish;
             while(!socket.isClosed()) {
                 
                 packet.setPacketNumber(packetNumber++);
+                packet.setTime(new Date());
                 
-                packetBytes = serializePacket(packet);                
-                
+                if(TEST_MODE == false) {
+                    packetBytes = serializePacket(packet);                
+                } else {
+                    packetBytes = new byte[]{1,2,3,4,5};
+                }
+                timeStart = System.nanoTime();                
                 os.write(packetBytes);
                 os.write("\r\n".getBytes());
                 os.flush();
 
                 List<Byte> bytes = new ArrayList<>();
                 byte receiveByte = 0;
-                while(receiveByte != -1) {
-                    receiveByte = (byte)is.read();                    
-                    if(receiveByte != -1) {
+                int intRead = 0;
+                while(intRead != -1) {
+                    intRead = is.read();
+                    receiveByte = (byte)intRead;                    
+                    if(intRead != -1) {
                         bytes.add(receiveByte);
                         if((bytes.size() > 2) &&
                            (bytes.get(bytes.size() - 2) == '\r') &&
                            (bytes.get(bytes.size() - 1) == '\n')) {
+//                            System.out.println("break");
                             break;
                         }
                     }
@@ -80,36 +121,48 @@ public class App
                 }
 
                 try {
-                    RawPacket resultPacket = deserializePacket(packetBytes);
-                    System.out.println(resultPacket);
-                } catch (ClassNotFoundException ex) {
-                    Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+                    
+                    if(TEST_MODE == false) {
+                        resultPacket = deserializePacket(packetBytes);
+                    } else {
+                        resultPacket = new RawPacket();
+                    }
+                    timeFinish = System.nanoTime();
+//                    System.out.println("TimeStart: " + timeStart + " ms.");
+//                    System.out.print("Received bytes: " + packetBytes.length);                    
+//                    System.out.println("TimeFinish: " + timeFinish + " ms.");
+                    System.out.println(resultPacket + "; time: " + ((timeFinish - timeStart)/1000) + " us.");
+                } catch (ClassNotFoundException | IOException ex) {
+                    logger.error(ExceptionUtils.getStackTrace(ex));
+                    for(byte b:bytes) {
+                        System.out.print(b);
+                    }
+                    System.out.println();
+                } finally {
                 }
-                
+//                System.out.println();
                 try {
-                    Thread.sleep(500);
+                    Thread.sleep(50);
                 } catch(InterruptedException ex) {
                     break;
                 }
             }
-        } catch (UnknownHostException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        } catch (UnknownHostException ex) {
+            logger.error(ExceptionUtils.getStackTrace(ex));
+        } catch (IOException ex) {
+            logger.error(ExceptionUtils.getStackTrace(ex));
         } finally {
             if(is != null)
                 try {
                     is.close();
             } catch (IOException ex) {
-                Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+                logger.error(ExceptionUtils.getStackTrace(ex));
             }
             if(os!= null)
                 try {
                     os.close();
             } catch (IOException ex) {
-                Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+                logger.error(ExceptionUtils.getStackTrace(ex));
             }
         }
         
@@ -154,6 +207,7 @@ public class App
                 if (in != null) {
                   in.close();
                 }
+                bis.close();
         }
         return resultPacket;
     }
