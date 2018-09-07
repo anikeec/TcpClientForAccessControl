023740f35f382ad07c34f15f076f9f23f68803fb
apu <pasha_anik@ukr.net>
2018-09-07 17:29:06 +0300

    Add logging

---
+++ src/main/java/com/apu/TcpClientForAccessControl/LogWriter.java
@@ -0,0 +1,17 @@
+package com.apu.TcpClientForAccessControl;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LogWriter {
+    
+    private static final Logger logger = LogManager.getLogger(LogWriter.class);
+    
+    public void writeToLog(byte[] message) {
+        String msg = null;
+        msg = "Logger Message: " + message;
+        logger.info(msg);
+//        return msg;
+    }
+
+}
